8b0496c0fa42547f3e1b53ffc215372773e188d0
David Molineus <david.molineus@netzmacht.de>
2020-10-19 16:37:20 +0200

    Cleanup code

--- src/BaseGeoJsonObject.php
+++ src/BaseGeoJsonObject.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Cowegis\GeoJson;
 
-
 use Cowegis\GeoJson\CoordinateReferenceSystem\CoordinateReferenceSystem;
 
 abstract class BaseGeoJsonObject implements GeoJsonObject
